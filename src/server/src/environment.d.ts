5471cd9b07a75c8d08702cc30070e34a54db7c26
NDPeshev19 <NDPeshev19@codingburgas.bg>
2023-11-28 10:56:06 +0200

    Add base server

---
+++ src/server/src/environment.d.ts
@@ -0,0 +1,20 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NODE_ENV: "development" | "production";
+
+            DATABASE_URI: string;
+            SERVER_HOST: string;
+
+            PAYLOAD_SECRET: string;
+
+            OAUTH_AUTHORIZATION_URL: string;
+            OAUTH_CLIENT_ID: string;
+            OAUTH_CLIENT_SECRET: string;
+            OAUTH_TOKEN_URL: string;
+            OAUTH_IDENTITY_ENDPOINT: string;
+        }
+    }
+}
+
+export {};
